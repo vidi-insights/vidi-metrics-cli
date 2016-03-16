5b4061e3b045a5194f0eecf1cfeedb6785530d2d
Jane Quinn <jane.quinn@nearform.com>
2016-03-16 12:31:08 +0000

    updated package.json, and catch errors with incorrect input

--- vidi-metrics-cli.js
+++ vidi-metrics-cli.js
@@ -1,16 +1,32 @@
 var Emitter = require("vidi-metrics-emitter")
 var jsonic = require("jsonic")
-var emit = Emitter()
+var isjson = require("is-json")
 var minimist = require("minimist")
 var inp = minimist(process.argv.slice(2))._[0]
-inp = jsonic.stringify(inp)
+if (isjson(inp)){
+  var emit = Emitter()
+  inp = jsonic.stringify(inp)
 
-if (inp && inp !== ''){
-  var input = function(inp) {
-  emit.emit(jsonic(inp))
+  if (inp && inp !== ''){
+    var input = function(inp) {
+    emit.emit(jsonic(inp))
+    }
+    input(inp)
+    setTimeout(function(){
+     process.exit()
+    }, 300)
   }
-  input(inp)
-  setTimeout(function(){
-   process.exit()
-  }, 300)
+}
+else {
+  try {
+    var json = jsonic(inp)
+    var emit = Emitter()
+    emit.emit(json)
+    setTimeout(function(){
+     process.exit()
+    }, 300)
+  }
+  catch (e) {
+    console.log(e)
+  }
 }
