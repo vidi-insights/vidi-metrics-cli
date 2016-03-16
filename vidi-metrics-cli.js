eef7e6e039c03b5842aa8e3287073ec27ea2816e
Jane Quinn <jane.quinn@nearform.com>
2016-03-16 11:00:30 +0000

    included jsonic, checks for valid json and empty string

--- vidi-metrics-cli.js
+++ vidi-metrics-cli.js
@@ -1,12 +1,16 @@
 var Emitter = require("vidi-metrics-emitter")
+var jsonic = require("jsonic")
 var emit = Emitter()
 var minimist = require("minimist")
 var inp = minimist(process.argv.slice(2))._[0]
+inp = jsonic.stringify(inp)
 
-var input = function(inp) {
-emit.emit(inp)
+if (inp && inp !== ''){
+  var input = function(inp) {
+  emit.emit(jsonic(inp))
+  }
+  input(inp)
+  setTimeout(function(){
+   process.exit()
+  }, 300)
 }
-input(inp)
-setTimeout(function(){
- process.exit()
-}, 300)
