69bef660cfa993482c37b278bd1038db337add34
Jane Quinn <jane.quinn@nearform.com>
2016-03-23 09:27:06 +0000

    codebase split into functions, timeouts removed

--- vidi-metrics-cli.js
+++ vidi-metrics-cli.js
@@ -2,31 +2,29 @@ var Emitter = require("vidi-metrics-emitter")
 var jsonic = require("jsonic")
 var isjson = require("is-json")
 var minimist = require("minimist")
-var inp = minimist(process.argv.slice(2))._[0]
-if (isjson(inp)){
-  var emit = Emitter()
-  inp = jsonic.stringify(inp)
+var input = minimist(process.argv.slice(2))._[0]
 
-  if (inp && inp !== ''){
-    var input = function(inp) {
-    emit.emit(jsonic(inp))
-    }
-    input(inp)
-    setTimeout(function(){
-     process.exit()
-    }, 300)
-  }
-}
-else {
-  try {
-    var json = jsonic(inp)
+function emit(inp){
+  if (isjson(inp)){
     var emit = Emitter()
-    emit.emit(json)
-    setTimeout(function(){
-     process.exit()
-    }, 300)
+    inp = jsonic.stringify(inp)
+
+    if (inp && inp !== ''){
+      var input = function(inp) {
+      emit.emit(jsonic(inp))
+      }
+      input(inp)
+    }
   }
-  catch (e) {
-    console.log(e)
+  else {
+    try {
+      var json = jsonic(inp)
+      var emit = Emitter()
+      emit.emit(json)
+    }
+    catch (e) {
+      console.log(e)
+    }
   }
 }
+emit(input)
