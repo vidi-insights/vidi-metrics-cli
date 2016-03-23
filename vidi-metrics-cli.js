aebda3c832db39c8760a0fab77c7be2db38c6d2e
Jane Quinn <jane.quinn@nearform.com>
2016-03-23 16:07:46 +0000

    initial version of interactive shell

--- vidi-metrics-cli.js
+++ vidi-metrics-cli.js
@@ -2,6 +2,7 @@ var Emitter = require("vidi-metrics-emitter")
 var jsonic = require("jsonic")
 var isjson = require("is-json")
 var minimist = require("minimist")
+var readLineSync = require("readline-sync")
 var input = minimist(process.argv.slice(2))._[0]
 
 function emit(inp){
@@ -27,4 +28,26 @@ function emit(inp){
     }
   }
 }
-emit(input)
+if(input == null) {
+  var json = {}
+  var name = readLineSync.question("Name for json? \n")
+  json.name = name
+  var values = readLineSync.question("Enter all values as a string in this form 'value1:a,value2:q' \n")
+  var tags = readLineSync.question("Enter all tags as a string in this form 'firstTag:tagGoesHere,secondTag:anotherTagGoesHere'\n")
+  var valuesArray = values.split(",")
+  var tagsArray = tags.split(",")
+  var localJson = {}
+  for (var i = 0; i < valuesArray.length; i++) {
+    localJson[valuesArray[i].substring(0,valuesArray[i].indexOf(":"))] = valuesArray[i].substring(valuesArray[i].indexOf(":")+1, valuesArray[i].length)
+    json.values = localJson
+  }
+  localJson = {}
+  for (var i = 0; i < tagsArray.length; i++) {
+    json[tagsArray[i].substring(0,tagsArray[i].indexOf(":"))] = tagsArray[i].substring(tagsArray[i].indexOf(":")+1, tagsArray[i].length)
+  }
+  var emit = Emitter()
+  emit.emit(json)
+}
+else {
+  emit(input)
+}
