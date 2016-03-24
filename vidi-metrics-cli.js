0f81ff9b5ff8fc9fb9fa6f24a7f51b1e6e0227b8
Jane Quinn <jane.quinn@nearform.com>
2016-03-24 16:03:17 +0000

    removed substrings and implemented jsonic

--- vidi-metrics-cli.js
+++ vidi-metrics-cli.js
@@ -36,18 +36,10 @@ if(input == null) {
   json.name = name
   var values = readLineSync.question("Enter all values as a string in this form 'value1:a,value2:q' \n")
   var tags = readLineSync.question("Enter all tags as a string in this form 'firstTag:tagGoesHere,secondTag:anotherTagGoesHere'\n")
-  var valuesArray = values.split(",")
-  var tagsArray = tags.split(",")
-  var localJson = {}
-  for (var i = 0; i < valuesArray.length; i++) {
-    localJson[valuesArray[i].substring(0,valuesArray[i].indexOf(":"))] = valuesArray[i].substring(valuesArray[i].indexOf(":")+1, valuesArray[i].length)
-    json.values = localJson
-  }
-  localJson = {}
-  for (var i = 0; i < tagsArray.length; i++) {
-    localJson[tagsArray[i].substring(0,tagsArray[i].indexOf(":"))] = tagsArray[i].substring(tagsArray[i].indexOf(":")+1, tagsArray[i].length)
-    json.tags = localJson
-  }
+  var valuesObj =  jsonic(values)
+  var tagsObj = jsonic(tags)
+  json.values = valuesObj
+  json.tags = tagsObj
   var emit = Emitter()
   emit.emit(json)
 }
